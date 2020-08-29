ce4e89314a9a7d611b624d02858fe66802a31b3d
krcstar <53501970+krcstar@users.noreply.github.com>
2020-08-29 17:26:43 +1200

    Update States.java

--- Group2-Project/src/org/compsys704/States.java
+++ Group2-Project/src/org/compsys704/States.java
@@ -2,7 +2,7 @@ package org.compsys704;
 
 public class States {
 
-	public static volatile boolean MOTOR = false;
+	public static volatile boolean MOTOR = true;
 	public static volatile boolean bottleLeft5 = true;
 	public static volatile boolean bottlePos1 = false;
 	public static volatile boolean bottlePos5 = !bottleLeft5;
